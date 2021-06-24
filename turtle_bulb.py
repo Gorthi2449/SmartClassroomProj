27bfe194860b99f38778b03d1c57476a2036aaa5
Vishal Gorthi <68380102+Gorthi2449@users.noreply.github.com>
2021-06-24 12:16:46 +0530

    Create turtle_bulb.py
    Creates a graphic window with a glowing yellow dot

---
+++ turtle_bulb.py
@@ -0,0 +1,9 @@
+import turtle
+def res2():
+    mlt1=turtle.Turtle()
+    mlt1.pen(shown=False)
+    mlt1.begin_fill()
+    mlt1.color('yellow')
+    mlt1.circle(50)
+    mlt1.end_fill()
+    turtle.done()
